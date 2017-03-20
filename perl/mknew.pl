c41879ff4d56fe2678f9c141a027f220e3d92384
caritofilaxy <caritofilaxy@gmail.com>
2017-03-20 21:17:57 +0300

    -

---
+++ perl/mknew.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use v5.022;
+use warnings;
+
+my $prj = $ARGV[0];
+$prj =~ s#$#.pl#;
+my $content = <<'END_CONTENT';
+#!/usr/bin/perl
+
+use v5.022;
+use warnings;
+END_CONTENT
+
+
+open(my $fh,'>',$prj) || die "Cant make new file: $!";
+print $fh $content;
+close($fh);
+chmod 0755, $prj;
+
