fdf615fd79e3794c898349b4114e7c3a15d572f7
caritofilaxy <caritofilaxy@gmail.com>
2017-03-30 04:20:30 +0300

    -

--- perl/alpaca/ref_array.pl
+++ perl/alpaca/ref_array.pl
@@ -3,21 +3,21 @@
 use strict;
 use warnings;
 
-my @regs = qw(eax ebx); 	# array
-my $ref_regs = \@regs;		# get ref 2 arr
+my @gen_regs = qw(eax ebx ecx edx);	# array
+my $ref_regs = \@gen_regs;		# get ref 2 arr
 my $copy_ref = $ref_regs;	# copy ref (to the same array, that is, same memory location);
 print $copy_ref."\n"; 		# print address of array
 my @deref = @{$copy_ref};	# dereferencing array;
 my @deref2 = @$copy_ref; 	# same and here i can avoid braces;
 my $deref_slice = $$copy_ref[1]; # slice gets $ptrs[1];
 $$ref_regs[2] = "ecx";		# add element to array;
-print "@regs\n";
+print "@gen_regs\n";
 
 my @ptrs = qw(esp ebp);
 my $ref_ptrs = \@ptrs;
 
 push @$ref_regs, @$ref_ptrs;
-print "@regs\n";
+print "@gen_regs\n";
 
 ##############################################
 my @regs_used = qw(ebx esi edi ebp);
@@ -41,3 +41,11 @@ chk_req(\@regs_used);
 
 print "@regs_used\n";
 ##############################################
+# nested arrays
+my @index = qw(esi edi);
+my @seg_regs = qw(cs ds ss);
+
+my @all_regs = (\@gen_regs, \@ptrs, \@index, \@seg_regs);
+my $all_regs_ref = \@all_regs;
+my $give_me_ptrs = @{$all_regs_ref}[1];
+print "@$give_me_ptrs\n";
