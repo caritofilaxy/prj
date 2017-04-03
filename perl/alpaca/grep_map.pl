0bb9250153f56f878a3fb435713f449b57a498d1
caritofilaxy <caritofilaxy@gmail.com>
2017-04-03 03:23:53 +0300

    -

--- perl/alpaca/grep_map.pl
+++ perl/alpaca/grep_map.pl
@@ -26,7 +26,28 @@ my %utils = (
 	#"net-p2p" => [ qw(amule bitcoind ctorrent dbhub freenet gnut transmission tribler) ],
 	"net-p2p" => [ qw(amule bitcoind transmission tribler) ],
 	"net-vpn" => [ qw(6tunnel tor vpnc vtun) ],
+	"dummy" => [ qw(avahi amule tor) ],
 );
 
 my @less_than_5 = grep { @{ $utils{$_} } < 5 } keys %utils;
-print "@who_has_less_than_5\n";
+print "@less_than_5\n";
+
+# suppose we have 2 arrays and we want elements (indexes) of @x that are larger than corresponding value in @y
+my @x = qw( 1 2 3 6 18 108 1944);
+my @y = qw( 1 2 4 8 16 32 64);
+
+my @bigger_indices = grep { 
+	($_ > $#y) or ($x[$_] > $y[$_]);
+	} 0..$#x;
+print "@bigger_indices\n";
+
+my @bigger_values = map { if ($_ > $#y or $x[$_] > $y[$_]) { $x[$_] } else { () }; } 0..$#x;
+print "@bigger_values\n";
+
+# from %utils who has "tor" ?
+my @hiders = grep { 
+		my @items = @{ $utils{$_} };
+		grep { $_ eq "tor"} @items;
+} keys %utils;
+
+print "@hiders\n";
