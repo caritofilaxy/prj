b2687aabd9975c758e0f6a9283df5737ccd309a4
Caritofilaxy <caritofilaxy@gmail.com>
2017-04-06 12:25:40 +0300

    -

--- perl/alpaca/callback/callback.pl
+++ perl/alpaca/callback/callback.pl
@@ -10,10 +10,22 @@ use File::Find;
 my @root_dir = "/home/aesin/git/prj/perl";
 # find($action, @root_dir);
 
-find ( sub { print "$File::Find::name found\n" }, @root_dir );
+#find ( sub { print "$File::Find::name found\n" }, @root_dir );
 
 ### closures
 
-my $total_size = 0;
-find ( sub { $total_size += -s if -f }, @root_dir );
-print $total_size."\n";
+#my $total_size = 0;
+#find ( sub { $total_size += -s if -f }, @root_dir );
+#print $total_size."\n";
+
+#The kind of subroutine that can access all lexical variables that existed at the time we
+#declared it is called a closure. In Perl
+#terms, a closure is a subroutine that references a lexical variable that has gone out of
+#scope.
+
+my $callback;
+{
+	my $count = 0;
+	$callback = sub { print  ++$count, ": $File::Find::name\n" };
+}										# at this moment $count eliminated.
+find($callback, @root_dir);
