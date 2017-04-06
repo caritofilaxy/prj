dab8f10dd3c577e841a891749625793e0619db18
Caritofilaxy <caritofilaxy@gmail.com>
2017-04-06 12:16:15 +0300

    -

--- perl/alpaca/callback/callback.pl
+++ perl/alpaca/callback/callback.pl
@@ -11,3 +11,9 @@ my @root_dir = "/home/aesin/git/prj/perl";
 # find($action, @root_dir);
 
 find ( sub { print "$File::Find::name found\n" }, @root_dir );
+
+### closures
+
+my $total_size = 0;
+find ( sub { $total_size += -s if -f }, @root_dir );
+print $total_size."\n";
