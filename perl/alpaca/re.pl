6b4c5c45134231697715a1fbd3c218e156125f55
caritofilaxy <caritofilaxy@gmail.com>
2017-04-12 16:38:02 +0300

    -

--- perl/alpaca/re.pl
+++ perl/alpaca/re.pl
@@ -14,7 +14,22 @@ say $d;
 
 # qr/abc123def/mi eq qr(?mi:abc123def)
 
-my $pirates = {
-	john => qr/(?i:long)?silver/i;
-	dog  => qr/black/i;
+#my $pirates = {
+#	john => qr/(?i:long)?silver/i;
+#	dog  => qr/black/i;
 	
+my $string = 'The quick brown fox jumps over lazy dog';
+if ($string =~ /quick/) {
+	my $pos0 = $-[0];
+	my $pos1 = $+[0];
+	say $string;
+	say "$pos0\t$pos1";
+}
+
+	
+if ($string =~ /brown\s(\w+?)\sjumps\s(\w+?)\slazy/) {
+	my $match0 = substr($string, $-[1], $+[1] - $-[1]); # $1;
+	say $match0;
+	my $match1 = $2; # substr($string, $-[2], $+[2] - $-[2]);
+	say $match1;
+}
