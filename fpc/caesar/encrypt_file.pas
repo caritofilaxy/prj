099c59433b4963ad2ea1d60afb50879dad956146
caritofilaxy <caritofilaxy@gmail.com>
2017-08-03 14:54:37 +0300

    -

--- fpc/caesar/encrypt_file.pas
+++ fpc/caesar/encrypt_file.pas
@@ -1,65 +1,73 @@
-{ ---=== encryption part ===--- }
-procedure encrypt_char(var c:char; offset: integer);
+const offset = 2;
+
+{ char2ord + offset, back2char }
+function encrypt_char(c: char): char;
 var
         i: integer;
 begin
-        i:=ord(c)+offset;
-        if i>126 then
-                i:=i-126+32;
-
-        c:=chr(i);
-
+	encrypt_char:=c;
+	if ((ord(c)>31) and (ord(c)<127)) then begin
+	        i:=ord(c)+offset;
+        	if i>126 then i:=i-126+31;
+	        encrypt_char:=char(i);
+	end;
 end;
 
-procedure encrypt_line(var d:string; offset: integer);
+{ same as above foreach char in string }
+procedure encrypt_line(var d:string);
 var
 	i: integer;
 begin
-	for i:=1 to length(d) do
-		encrypt_char(d[i], offset);
+	for i:=1 to length(d) do d[i]:=encrypt_char(d[i]);
 end;
 
 
 { ---=== encryption part ===--- }
 
 { ---=== decryption part ===--- }
-procedure decrypt_char(var c:char; offset: integer);
+function decrypt_char(c:char);
 var
         i: integer;
 begin
-        i:=ord(c)-offset;
-        if i<32 then
-                i:=i+126-32;
-
-        c:=chr(i);
+	decrypt_char:=c;
+	if ((ord(c)>31) and (ord(c)<127)) then begin
+	        i:=ord(c)-offset;
+	        if i<32 then  i:=i+126-31;
+	        decrypt_char:=char(i);
+	end;
 end;
 
 procedure decrypt_line(var d:string; offset: integer);
 var
 	i: integer;
 begin
-	for i:=1 to length(d) do
-		decrypt_char(d[i], offset);
+	for i:=1 to length(d) do d[i] decrypt_char(d[i]);
 end;
 
 
 { ---=== decryption part ===--- }
 
+{ ---=== file encryption ===--- }
+procedure encrypt_file(const aFile: string; aOper: boolean);
+var
+	FileIn: text;
+	FileOut: text;
+	S, I, O: string;
+
+begin
+	write('File to operate: '); 
+
 
 { ---=== main ===--- }
 
 var
-	F,T: text;
-	S: string;
+	n: integer;
 
 begin
+	writeln('choose operaion: ');
+	writeln('1 - encrypt');
+	writeln('2 - decrypt');
+	write('enter number from above: '); readln(n);
 
-	assign(F,'dummy.txt'); reset(F);
-	assign(T,'dummy.txt.encrypted'); rewrite(T);
-
-	while not eof(F) do begin
-		readln(F, S);
-		encrypt_line(S, 1);
-		writeln(T, S);
-	end;
-end.
+	case n of:
+		1) 
