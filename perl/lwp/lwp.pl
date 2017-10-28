40aa00b461dd2a8a4f0ce2484ab0343e4e6144d1
caritofilaxy <caritofilaxy@gmail.com>
2017-10-28 09:39:45 +0300

    -

--- perl/lwp/lwp.pl
+++ perl/lwp/lwp.pl
@@ -5,14 +5,21 @@ use warnings;
 use v5.22;
 
 use LWP::UserAgent;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 my $separator = "============================";
 
 my $ua = LWP::UserAgent->new();
 $ua->agent("Figzilla 52.7");
 
-my $response = $ua->get("http://www.rbc.ru");
+my $t0 = [gettimeofday];
+my $response = $ua->get("http://www.news.com");
+my $t1 = tv_interval ($t0, [gettimeofday]);
+print "fetched for $t1 seconds\n";
+$t0 = [gettimeofday];
 my $text = $response->content;
+my $t2 = tv_interval($t0, [gettimeofday]);
+printf "content generated for $t2 seconds\n";
 
 
 my @lines = split("\n", $text);
