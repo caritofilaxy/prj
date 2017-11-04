5d6660dcb68094de5c70ba21844ee4b9b871fc5b
Caritofilaxy <caritofilaxy@gmail.com>
2017-11-04 19:56:57 +0300

    -

--- C/arr_vs_poi.c
+++ C/arr_vs_poi.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
- void func(char subarray[100],char* pointer)
- {
+
+/*
+void func(char subarray[100],char* pointer) {
        printf("sizeof subarray=%zd\n", sizeof(subarray));
        printf("address of subarray=%p\n", (void *)subarray);
        printf("pointer            =%p\n", (void *)pointer);
- }
- int main()
- {
+}
+
+
+int main() {
        char array[100];
        printf("sizeof of array=%zd\n", sizeof(array));
        printf("address of array=%p\n", (void *)array);
        printf("address of array[0]=%p\n", (void *)&array[0]);
        func(array,array);
- }
-        
+
+	   printf("%c %d\n", array[3], arr[3]);
+} 
+*/
+
+
+int main(void) {
+
+	   char charr[5];
+	   int intarr[5];
+	   
+	   *(charr+3) = 'A';
+	   *(intall+3) = 44;
+
+		
